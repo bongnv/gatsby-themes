2214fa7c71da7abb86d358aa98b75f03c1c31e91
Bong Nguyen <van.bong@grabtaxi.com>
2020-11-11 10:00:37 +0800

    feat: enable mdx

--- gatsby-theme-blog/gatsby-config.js
+++ gatsby-theme-blog/gatsby-config.js
@@ -1,3 +1,31 @@
 module.exports = {
-  plugins: ["gatsby-plugin-theme-ui"],
+  plugins: [
+    "gatsby-plugin-theme-ui",
+    {
+      resolve: "gatsby-plugin-mdx",
+      options: {
+        extensions: [".mdx", ".md"],
+      },
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "posts",
+        path: "src/posts",
+      },
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "src/pages",
+      },
+    },
+    {
+      resolve: "gatsby-plugin-page-creator",
+      options: {
+        path: "src/posts",
+      },
+    },
+  ],
 };
